320c4c471cb2c2cd733d36b455d96c7bc2ba7dc3
Azamah Jr <depixellife@gmail.com>
2023-12-08 18:50:41 +0000

    Added: Lambda stack

---
+++ src/infrastructure/stacks/lambdaStack.ts
@@ -0,0 +1,48 @@
+import { Stack, StackProps } from "aws-cdk-lib";
+import { LambdaIntegration } from "aws-cdk-lib/aws-apigateway";
+import { ITable } from "aws-cdk-lib/aws-dynamodb";
+import { Effect, PolicyStatement } from "aws-cdk-lib/aws-iam";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Construct } from "constructs";
+import { join } from "path";
+
+
+interface LambdaStackProps extends StackProps {
+    passwordsTable: ITable
+}
+
+export class LambdaStack extends Stack {
+    public readonly passwordLambdaIntegration: LambdaIntegration
+
+    constructor(scope: Construct, id: string, props: LambdaStackProps) {
+        super(scope, id, props);
+
+
+        const passwordsLambda = new NodejsFunction(this, 'PasswordsLambda', {
+            runtime: Runtime.NODEJS_18_X,
+            handler: 'handler',
+            entry: (join(__dirname, '..', '..' ,'services', 'tools','handler.ts')),
+            environment: {
+                TABLE_NAME: props.passwordsTable.tableName
+            }
+        })
+   
+
+        passwordsLambda.addToRolePolicy(new PolicyStatement({
+          
+            effect: Effect.ALLOW,
+            resources: [props.passwordsTable.tableArn],
+            actions:[
+                'dynamodb:PutItem',
+                'dynamodb:Scan',
+                'dynamodb:GetItem',
+                'dynamodb:UpdateItem',
+                'dynamodb:DeleteItem'
+            ]
+        }))
+
+
+        this.passwordLambdaIntegration = new LambdaIntegration(passwordsLambda)
+    }
+}
